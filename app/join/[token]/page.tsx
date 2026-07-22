36026ba78144b11283286627952b4047a4e81131
Fry1997 <128788997+Fry1997@users.noreply.github.com>
2026-07-22 01:19:26 +0100

    Add household invitation join route

---
+++ app/join/[token]/page.tsx
@@ -0,0 +1,10 @@
+import { JoinHousehold } from "@/components/JoinHousehold";
+
+export default async function JoinPage({
+  params,
+}: {
+  params: Promise<{ token: string }>;
+}) {
+  const { token } = await params;
+  return <JoinHousehold token={token} />;
+}
